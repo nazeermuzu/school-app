22dac39ef504440d692babefe1b950e09c218e57
Atheif <sheikathiefathief@gmail.com>
2025-01-23 19:47:50 +0530

    added logs

---
+++ lib/logs/todos.dart
@@ -0,0 +1,7 @@
+// if you get a merge error while entering the git pull command
+// try the below followings
+
+// 1. click 'i' to enable edit
+// 2. type any message accordingly
+// 3. after all, click 'esc' button, then ':wq!'
+// 4. then check again 'git pull' to verify all the changes are up to date
